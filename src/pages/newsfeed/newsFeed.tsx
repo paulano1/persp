9f7e670e8e2d5a653cefb4de4d3836052033cd42
Anomitro Paul <anomitropaul@Anomitros-MacBook-Pro.local>
2023-04-08 10:02:13 -0400

    infinite scrolling

--- src/pages/newsfeed/newsFeed.tsx
+++ src/pages/newsfeed/newsFeed.tsx
@@ -1,27 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TokenGroup } from "./tokenGroup";
 import { Layout } from "../layout/layout";
 import { NewsContainer } from "./newsContainer";
 import './index.css'
 
-const data = [
-    'hello',
-    'world',
-    'hello1',
-    'world1',
-    'hello2',
-    'world2',
-    'hello3',
-]
-const news = {
-    title: "What could Trump’s arraignment mean for his political future?",
-    description: "Former President Trump was charged with 34 felony counts today – but what could this mean for the future of his political career? NBC News’ Kristen Welker reports on the unprecedented shakeup for the race to the White House in 2024.",
-    image: "https://www.usnews.com/object/image/00000187-3996-d0b6-a7e7-b9f67e3a0000/ap23064025376604.jpg?update-time=1680298477110&size=responsive640",
-    url: "https://www.usnews.com/news/politics/articles/2021-01-13/what-could-trumps-arraignment-mean-for-his-political-future",
-    whyGotRecommended: "You might be interested in this article because you are interested in politics."
+interface News {
+    title: string;
+    description: string;
+    image: string;
+    url: string;
+    whyGotRecommended: string;
 }
 
 export const NewsFeed = () => {
+    const [newsList, setNewsList] = useState<News[]>([]);
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        fetchNews();
+    }, []);
+
+    const fetchNews = async () => {
+        setIsLoading(true);
+        const response = returnMockNews()
+        const data = response
+        setNewsList(prevList => [...prevList, ...data]);
+        setIsLoading(false);
+    };
+
+    const handleScroll: React.UIEventHandler<HTMLDivElement> = (event) => {
+        const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+        if (scrollTop === 0 && !isLoading) {
+            setPage(prevPage => prevPage + 1);
+            fetchNews();
+        }
+    };
 
     return (
         <Layout>
@@ -34,18 +48,49 @@ export const NewsFeed = () => {
 
                 </p>
             </div>
-            <div>
-            <NewsContainer
-                title={news.title}
-                description={news.description}
-                image={news.image}
-                url={news.url}
-                whyGotRecommended={news.whyGotRecommended}
-            />
-        </div>
-        
+            <div onScroll={handleScroll} style={{ overflowY: "scroll", height: "calc(100vh - 200px)" }}>
+                {newsList.map((newsItem, index) => (
+                    <NewsContainer
+                        key={index}
+                        title={newsItem.title}
+                        description={newsItem.description}
+                        image={newsItem.image}
+                        url={newsItem.url}
+                        whyGotRecommended={newsItem.whyGotRecommended}
+                    />
+                ))}
+                {isLoading && <p>Loading...</p>}
+            </div>
         </Layout>
     );
 }
 
 
+
+
+const data = [
+    'hello',
+    'world',
+    'hello1',
+    'world1',
+    'hello2',
+    'world2',
+    'hello3',
+]
+
+function returnMockNews(): News[] {
+    return Array.from({ length: 10 }, (_, index) => ({
+        title: `News ${index}`,
+        description: `Description ${index}`,
+        image: `https://picsum.photos/200/300?random=${index}`,
+        url: `https://www.google.com/search?q=${index}`,
+        whyGotRecommended: `You might be interested in this article because you are interested in ${data[index]}.`
+    }));
+}
+const news = {
+    title: "What could Trump’s arraignment mean for his political future?",
+    description: "Former President Trump was charged with 34 felony counts today – but what could this mean for the future of his political career? NBC News’ Kristen Welker reports on the unprecedented shakeup for the race to the White House in 2024.",
+    image: "https://www.usnews.com/object/image/00000187-3996-d0b6-a7e7-b9f67e3a0000/ap23064025376604.jpg?update-time=1680298477110&size=responsive640",
+    url: "https://www.usnews.com/news/politics/articles/2021-01-13/what-could-trumps-arraignment-mean-for-his-political-future",
+    whyGotRecommended: "You might be interested in this article because you are interested in politics."
+}
